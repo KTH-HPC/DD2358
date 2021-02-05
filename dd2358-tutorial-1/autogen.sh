db66973535c872ff58cedc9bbf6945eaa23429b2
steven <wdchien@kth.se>
2021-02-05 11:04:08 +0100

    autogen shortcut exit

--- dd2358-tutorial-1/autogen.sh
+++ dd2358-tutorial-1/autogen.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # 1. collect the m4 macros required by configure.ac
-aclocal
+aclocal || exit 1
 
 # 2. generate Makefile.in templates from Makefile.am
-automake --add-missing
+automake --add-missing || exit 1
 
 # 3. generate configure script from configure.ac
-autoreconf -i -f
+autoreconf -i -f || exit 1
