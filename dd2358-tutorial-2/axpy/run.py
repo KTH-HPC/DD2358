6d14f00331464dabb9fb05452c09a8a1dea9dbbb
steven <wdchien@kth.se>
2021-02-11 21:12:50 +0100

    ignore shared library and display data types

--- dd2358-tutorial-2/axpy/run.py
+++ dd2358-tutorial-2/axpy/run.py
@@ -6,10 +6,14 @@ y = np.random.random(5).astype(np.float32)
 a = 0.5
 z = a * x + y
 math_kernels.axpy(a, x, y)
+print(z, z.dtype)
+print(y, y.dtype)
 print(y - z)
 
 x = np.random.random(5).astype(np.float64)
 y = np.random.random(5).astype(np.float64)
 z = a * x + y
 math_kernels.axpy(a, x, y)
+print(z, z.dtype)
+print(y, y.dtype)
 print(y - z)
