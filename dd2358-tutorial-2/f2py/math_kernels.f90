ef9dc44d645185e82203d642ee53b78d9a023c01
steven <wdchien@kth.se>
2021-02-11 21:10:40 +0100

    restore full file

--- dd2358-tutorial-2/f2py/math_kernels.f90
+++ dd2358-tutorial-2/f2py/math_kernels.f90
@@ -9,3 +9,33 @@ contains
     y = a * x + y
   end subroutine
 end module kernels
+
+program main
+  use, intrinsic :: iso_fortran_env, only : int32, real64
+  use kernels
+  implicit none
+  real(kind=real64)                            :: a
+  real(kind=real64), allocatable, dimension(:) :: x
+  real(kind=real64), allocatable, dimension(:) :: y
+
+  integer(kind=int32) :: length = 10
+  integer(kind=int32) :: i = 10
+  allocate(x(length))
+  allocate(y(length))
+
+  a = 0.8
+
+  do i = 1, length
+    x(i) = 0.5 * i
+    y(i) = 0.1 * i
+  end do
+
+  do i = 1, length
+    print *, a * x(i) + y(i)
+  end do
+  call axpy(a, x, y)
+  print *, y(:)
+
+  deallocate(x)
+  deallocate(y)
+end program
